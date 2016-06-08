88eaecf37f6dfa5131dee592024988170005ac0f
pegerto <pegerto@ip-172-16-4-132.eu-west-1.compute.internal>
2016-06-08 20:56:46 +0100

    first commit

---
+++ rabbitmq-consul.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/hashicorp/consul/api"
+	"gopkg.in/yaml.v2"
+)
+
+type BridgeConfig struct {
+	Service Service
+}
+
+type Service struct {
+	Name    string
+	Port    int
+	Address string
+}
+
+func main() {
+
+	var config_file = flag.String("config_file", "", "Configuration file for the bridge")
+	required := []string{"config_file"}
+	flag.Parse()
+
+	seen := make(map[string]bool)
+	flag.Visit(func(f *flag.Flag) { seen[f.Name] = true })
+	for _, req := range required {
+		if !seen[req] {
+			fmt.Fprintf(os.Stderr, "missing required -%s argument\n", req)
+			os.Exit(2)
+		}
+	}
+
+	if _, err := os.Stat(*config_file); os.IsNotExist(err) {
+		fmt.Fprintf(os.Stderr, "configuration file  -%s do not exists\n", *config_file)
+		os.Exit(2)
+	}
+
+	file, e := ioutil.ReadFile(*config_file)
+	if e != nil {
+		fmt.Printf("File error: %v\n", e)
+		os.Exit(1)
+	}
+
+	var config BridgeConfig
+	err := yaml.Unmarshal(file, &config)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	client, err := api.NewClient(api.DefaultConfig())
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(config.Service.Address)
+	agent := client.Agent()
+	service := &api.AgentServiceRegistration{ID: config.Service.Name,
+		Name:    config.Service.Name,
+		Tags:    nil,
+		Port:    config.Service.Port,
+		Address: config.Service.Address}
+
+	agent.ServiceRegister(service)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+}
